c6947060183dd028499e92c3d09412a92703493a
mahelhelou <mahelhelou@gmail.com>
2022-06-14 17:17:03 +0300

    Fixing the broken links

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ function styles() {
 function scripts() {
 	const jsPath = {
 		jquery: "app/assets/scripts/libs/jquery.min.js",
-		popper: "app/assets/scripts/libs/popper.min.js",
+		// popper: "app/assets/scripts/libs/popper.min.js",
 		bootstrap: "app/assets/scripts/libs/bootstrap.min.js",
 		owl: "app/assets/scripts/libs/owl.carousel.min.js",
 		app: "app/assets/scripts/app.js",
